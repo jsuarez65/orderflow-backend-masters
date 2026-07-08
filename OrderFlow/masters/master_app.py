17c397b8df370dfbad833fecdb4a2562eba4f176
cbasrc <cbasrc1701@gmail.com>
2026-07-08 20:35:41 -0300

    fix revision

--- OrderFlow/masters/master_app.py
+++ OrderFlow/masters/master_app.py
@@ -3,16 +3,16 @@ from flask_cors import CORS
 from configuration.LogConfiguration import LogConfiguration
 from controllers.ProductController import ProductBlueprint
 from controllers.ProviderController import ProviderBlueprint
+from controllers.CustomerController import CustomerBlueprint
 
 masterMain = Flask(__name__)
 CORS(masterMain)
 
 masterMain.register_blueprint(ProductBlueprint)
 masterMain.register_blueprint(ProviderBlueprint)
+masterMain.register_blueprint(CustomerBlueprint)
 
 LogConfiguration.configure()
 
 if __name__ == '__main__':
     masterMain.run(debug=True)
-
-
